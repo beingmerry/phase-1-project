c6e9684b97cb60b72dc26dfbe9cb1434735dcf72
Ben Merryman <ben.merryman@gmail.com>
2022-11-14 14:47:21 -0500

    bugged

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// 🏗️ Import list of dependencies
+import * as dotenv from 'dotenv'; 
+import express from 'express';
+dotenv.config();
+
+
+fetch(`https://developer.nps.gov/api/v1/parks?parkCode=yell&api_key=sp0dnsxYnjFUifpp4qWTzlyai6F4p6q547rYujf7`)
+.then(response => response.json)
+.then(parks => {
+  console.log(parks)
+})
 //JS code below
