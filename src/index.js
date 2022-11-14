2442702e8bbf2ebb629d1223a2f33729bf4af2d4
Ben Merryman <ben.merryman@gmail.com>
2022-11-14 15:26:11 -0500

    testing import dotEnv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 // 🏗️ Import list of dependencies
-import * as dotenv from 'dotenv'; 
-import express from 'express';
-dotenv.config();
+import dotenv from 'dotenv';
 
-
-fetch(`https://developer.nps.gov/api/v1/parks?parkCode=yell&api_key=sp0dnsxYnjFUifpp4qWTzlyai6F4p6q547rYujf7`)
-.then(response => response.json)
-.then(parks => {
-  console.log(parks)
+fetch(`https://developer.nps.gov/api/v1/parks?parkCode=yell&api_key=${API_KEY}`)
+.then(response => response.json())
+.then(park => {
+  console.log(park.data[0].fullName)
+  const h1 = document.querySelector('h1')
+  h1.textContent = park.data[0].fullName
 })
 //JS code below
