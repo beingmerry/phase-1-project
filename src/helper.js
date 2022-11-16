f6912227bf009f870e3c1c743c74ac4ae8725d2b
Ben Merryman <ben.merryman@gmail.com>
2022-11-16 15:13:58 -0500

    switched from list to table style with Bootstrap

--- src/helper.js
+++ src/helper.js
@@ -1,87 +1,103 @@
 // 🎯🟢 Fetch the breweries near you! Boulder by default
 
-const cityFetch = (city="Boulder") =>{
-    fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}`)
-    .then(response => response.json())
-    .then(breweries => {
-       if(breweries.length === 0){ 
-        listBreweries.textContent = "No Breweries Found"
-       } else {
-        loadSearchResults(breweries)
-       }
-    })
-    .catch((error)=> {
-        console.error(error)
-        listBreweries.textContent = "API SERVER ERROR"
-    })   
-}
+const cityFetch = (city = "Boulder") => {
+    fetch(`https://api.openbrewerydb.org/breweries?by_city=${city}&per_page=10`)
+        .then((response) => response.json())
+        .then((breweries) => {
+            if (breweries.length === 0) {
+                errorMessage.textContent = "No Breweries Found";
+            } else {
+                loadSearchResults(breweries);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            errorMessage.textContent = "API SERVER ERROR";
+        });
+};
 
 // 🎯 Go to local db and grab favorites list, render at page load
-const favoritesFetch = () =>{
-    myBreweriesList.innerHTML = ""
+const favoritesFetch = () => {
+    myBreweriesList.innerHTML = "";
     fetch(`http://localhost:3000/myBreweryList`)
-    .then(response => response.json())
-    .then(myBreweries => myBreweries.forEach(brewery => renderFavorite(brewery.name)))
-    .catch((error)=> console.error(error))   
-}
+        .then((response) => response.json())
+        .then((myBreweries) =>
+            myBreweries.forEach((brewery) => renderFavorite(brewery.name))
+        )
+        .catch((error) => console.error(error));
+};
 
 const loadSearchResults = (breweries) => {
-    listBreweries.innerHTML = ""
-    allBreweries = [...breweries]
-    breweries.forEach(brewery => {  
-      const newBreweryListElement = document.createElement('li')
-      newBreweryListElement.textContent = brewery.name
-      newBreweryListElement.id          = `${brewery.id}`
-      newBreweryListElement.classList.add('brewery-list-element')
-      newBreweryListElement.addEventListener('click', ()=>renderBrewery(brewery))
-      listBreweries.appendChild(newBreweryListElement)
-    });
-    renderBrewery(breweries[0])
-  }
+    breweryResultsTable.innerHTML = "";
+    allBreweries = [...breweries];
+    breweries.forEach((brewery) => renderBreweryRow(brewery));
+    renderBrewery(breweries[0]);
+};
+const renderBreweryRow = (brewery) => {
+    const newBreweryRowElement = document.createElement("tr");
+    const firstElementResultNumber = 1 + 10 * currentPage;
+    newBreweryRowElement.classList.add("brewery-row-result");
+    const tableRowCount = document.querySelectorAll(".brewery-row-result").length;
+    const currentResultNumber = tableRowCount + firstElementResultNumber;
+    const phoneNumberCheck = (brewery.phone !== null ? brewery.phone : "N/A")
+    newBreweryRowElement.innerHTML = `
+        <td><button id="see-brewery-details-${currentResultNumber}">➕</button></td>
+        <td>${currentResultNumber}</td>
+        <td>${brewery.name}</td>
+        <td>${brewery.brewery_type}</td>
+        <td>${brewery.street}, ${brewery.city}, ${brewery.state} ${brewery.postal_code}</td>
+        <td>${phoneNumberCheck}</td>
+    `;
+    newBreweryRowElement.id          = `${brewery.id}`
+    newBreweryRowElement.classList.add('brewery-list-element')
+    // const detailButton = document.querySelector(`#see-brewery-details-${currentResultNumber}`)
+    // 🎯 The detail button is not being found at runtime ?? ⚠️
+    // detailButton.addEventListener('click', ()=>renderBrewery(brewery))
+    breweryResultsTable.appendChild(newBreweryRowElement);
+};
 
+const renderBrewery = (brewery) => {
+    currentBrewery.breweryApiId = brewery.id;
+    currentBrewery.name = brewery.name;
+    currentBrewery.breweryType = brewery.brewery_type.toUpperCase();
+    currentBrewery.breweryFullAddress = `${brewery.street}, ${brewery.city}, ${brewery.state} ${brewery.postal_code}`;
+    currentBrewery.url = brewery.website_url;
 
-  const renderBrewery = (brewery)=>{
-    currentBrewery.breweryApiId       = brewery.id
-    currentBrewery.name               = brewery.name
-    currentBrewery.breweryType        = brewery.brewery_type.toUpperCase()
-    currentBrewery.breweryFullAddress = `${brewery.street}, ${brewery.city}, ${brewery.state} ${brewery.postal_code}`
-    currentBrewery.url                = brewery.website_url
+    breweryName.dataset.id = currentBrewery.breweryApiId;
+    breweryName.textContent = currentBrewery.name;
+    breweryType.textContent = currentBrewery.breweryType;
+    breweryAddress.textContent = currentBrewery.breweryFullAddress;
+    brewerySite.textContent = currentBrewery.url;
+    brewerySite.href = currentBrewery.url;
+};
 
-    breweryName.dataset.id     = currentBrewery.breweryApiId      
-    breweryName.textContent    = currentBrewery.name              
-    breweryType.textContent    = currentBrewery.breweryType       
-    breweryAddress.textContent = currentBrewery.breweryFullAddress
-    brewerySite.textContent    = currentBrewery.url               
-    brewerySite.href           = currentBrewery.url
-}
-
-const renderFavorite = (breweryName) =>{
-    const newBrewery = document.createElement('li')
-    const idValue    = myBreweriesList.children.length + 1
-    newBrewery.classList.add('my-brewery-list-element')
-    newBrewery.id = `my-brewery-${idValue}`
+const renderFavorite = (breweryName) => {
+    const newBrewery = document.createElement("li");
+    const idValue = myBreweriesList.children.length + 1;
+    newBrewery.classList.add("my-brewery-list-element");
+    newBrewery.id = `my-brewery-${idValue}`;
     //brewery name is calling a global level variable and needs to call an argument
-    newBrewery.textContent = breweryName
-    favorites = [...favorites, breweryName]
-    myBreweriesList.appendChild(newBrewery)
-}
+    newBrewery.textContent = breweryName;
+    favorites = [...favorites, breweryName];
+    myBreweriesList.appendChild(newBrewery);
+};
 
-const postNewBrewery = ()=>{
+const postNewBrewery = () => {
     // 🎯🚩 Check current favorites before posting duplicate to db.json
-    if (0 === favorites.filter(favorite => favorite===currentBrewery.name).length) {
-        fetch(`http://localhost:3000/myBreweryList`,{
-            method:`POST`,
+    if (
+        0 ===
+        favorites.filter((favorite) => favorite === currentBrewery.name).length
+    ) {
+        fetch(`http://localhost:3000/myBreweryList`, {
+            method: `POST`,
             headers: {
-                'Content-Type': 'application/json',
-                Accept : 'application/json'
+                "Content-Type": "application/json",
+                Accept: "application/json",
             },
-            body: JSON.stringify(currentBrewery) 
+            body: JSON.stringify(currentBrewery),
         })
-        .then(response=> response.json())
-        .then(breweryAdded => {
-            console.log(breweryAdded)
-            renderFavorite(breweryName.textContent)
-        })
-        .catch(err => console.error(err))
+            .then((response) => response.json())
+            .then((breweryAdded) => renderFavorite(breweryName.textContent))
+            .catch((err) => console.error(err));
     }
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
-// 🌐 Global Variables
-let allBreweries   = [];
-let currentBrewery = {};
-let favorites      = [];
-
-const listBreweries = document.querySelector('ol#list-breweries');
-
-const breweryName       = document.querySelector('#brewery-name');
-const breweryType       = document.querySelector('#brewery-type');
-const breweryAddress    = document.querySelector('#brewery-address');
-const brewerySite       = document.querySelector('#brewery-url');
-const brewerySearchForm = document.querySelector('#search-form');
-
-const myBreweriesList  = document.querySelector('#my-breweries-list');
-const breweryAddButton = document.querySelector('#add-brewery-button')
-
-// ⚠️ Warnings ⚠️
-// ⚠️ Helper functions are located in the helper.js file ⚠️
+// ⚠️ ReadMe First! ⚠️
+//  🔨  1. Helper functions are located in the helper.js file 🔨
+//  🌐  2. Global variables are located in the global.js file 🌐
 // 🏗️ Current Tasks
-// 🎯 1. Flesh out the rest of the HTML to have all elements (only create if required)
-// 🎯 2. Build Basic CSS to spit the screen into it's space (top bar, 2 columns, footer, etc.)
-// 🎯 3. Construct JS to perform search function for city
-// 🎯 4. Build the like function into each brewery, when liked, load that brewery into local db
-// 🎯 5. Build the "Add to my bar hop" function
+//  🎯🚩1. Flesh out the rest of the HTML elements
+//  🎯🚩 2. Build Basic CSS (top bar, 2 columns, footer) w/ Bootstrap styles
+//  🎯🚩 3. Construct JS to perform search function for city
+//  🎯🚩 4. Build the like function into each brewery, when liked, load that brewery into local db
+//  🎯🚩 5. Build the "Add to my bar hop" function
 
 // 🥇 Initialize all scripts, forms, and event listeners
-
-
 const init = () => {
   // 📕 All init function on this page, all other functions in helper.js
   initBrewerySearchForm()
@@ -50,6 +33,5 @@ const initBreweryAddButton = () => {
     postNewBrewery()
   })
 }
-
 // ⌛ Wait until document loaded to run initialization
 document.addEventListener('DOMContentLoaded', init)
